3127d88284aad66b6ec94e660ffcddc223b7eb64
Elena Gromova <elena_gromova1@epam.com>
2023-10-26 21:54:55 +0200

    homework_10_hw1

---
+++ homework_10/hw1.py
@@ -0,0 +1,55 @@
+"""
+Vasya implemented non-optimal Enum classes.
+Remove duplications in variables declarations using metaclasses.
+
+from enum import Enum
+
+
+class ColorsEnum(Enum):
+    RED = "RED"
+    BLUE = "BLUE"
+    ORANGE = "ORANGE"
+    BLACK = "BLACK"
+
+
+class SizesEnum(Enum):
+    XL = "XL"
+    L = "L"
+    M = "M"
+    S = "S"
+    XS = "XS"
+
+
+Should become:
+
+class ColorsEnum(metaclass=SimplifiedEnum):
+    __keys = ("RED", "BLUE", "ORANGE", "BLACK")
+
+
+class SizesEnum(metaclass=SimplifiedEnum):
+    __keys = ("XL", "L", "M", "S", "XS")
+
+
+assert ColorsEnum.RED == "RED"
+assert SizesEnum.XL == "XL"
+"""
+
+
+class SimplifiedEnum(type):
+    def __new__(cls, name, bases, dct):
+        keys = dct.get('_{}__keys'.format(name), ())
+        dct.update({key: key for key in keys})
+        cls_instance = super().__new__(cls, name, bases, dct)
+        return cls_instance
+
+
+class ColorsEnum(metaclass=SimplifiedEnum):
+    __keys = ("RED", "BLUE", "ORANGE", "BLACK")
+
+
+class SizesEnum(metaclass=SimplifiedEnum):
+    __keys = ("XL", "L", "M", "S", "XS")
+
+
+assert ColorsEnum.RED == "RED"
+assert SizesEnum.XL == "XL"
